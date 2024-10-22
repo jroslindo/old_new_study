044a69d4a94c8b6d10f8bc05353cf2db4f26c353
jroslindo <jroslindotab@gmail.com>
2024-10-22 11:34:09 -0300

    changes

--- codechecker.py
+++ codechecker.py
@@ -1,16 +1,41 @@
 import argparse
+import os
+
+CODECHECKER_TEXT = """
+{
+	"analyze": [
+		"--disable-all",
+		"--enable=bugprone-unchecked-optional-access",
+		"--jobs=10",
+		"--clean",
+		"--ignore=skipfiles"
+	],
+	"parse": [
+		"--ignore=skipfiles"
+	]
+}
+"""
+
 
 def execute_github_codechecker(files:str):
+    BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
     #splitting files and filtering for .cpp
     files = files.split(" ")
     files = [file for file in files if file.find(".cpp") > 0]
 
     #writing skipfiles
-    arq = open("/mnt/extended/old_new_study/skpifiles", "w")
+    arq = open(f"{BASE_PATH}/skpifiles", "w")
     for file in files:
         arq.write(f"+*/{file}\n")
     arq.write("-*/*\n")
     arq.close()
+
+    
+    if not os.path.isfile(f"{BASE_PATH}/codechecker.json"):
+        arq = open(f"{BASE_PATH}/codechecker.json", "w")
+        arq.write(CODECHECKER_TEXT)
+        arq.close()
 
 def main(args):
     if args.github_action and args.files !="":

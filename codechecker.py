e1920ebfb4c1722b60950e11f1042acf2c54b87f
jroslindo <jroslindotab@gmail.com>
2024-10-22 11:19:43 -0300

    change

--- codechecker.py
+++ codechecker.py
@@ -1,7 +1,20 @@
 import argparse
-import subprocess
+
+def execute_github_codechecker(files:str):
+    #splitting files and filtering for .cpp
+    files = files.split(" ")
+    files = [file for file in files if file.find(".cpp") > 0]
+
+    #writing skipfiles
+    arq = open("skpifiles", "w")
+    for file in files:
+        arq.write(f"+*/{file}\n")
+    arq.write("-*/*\n")
+    arq.close()
 
 def main(args):
+    if args.github_action and args.files !="":
+        execute_github_codechecker(args.files)
     print(args)
 
 if __name__ == "__main__":
@@ -9,8 +22,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Teste")
     
     # Add arguments
-    parser.add_argument('--github-action', type=bool, help='Is it github actions?', default=False, required=False)
-    parser.add_argument('--compare-branch', type=str, help='Branch the PR will be open to', default="", required=False)
+    parser.add_argument('--github_action', type=bool, help='Is it github actions?', default=False, required=False)
+    parser.add_argument('--compare_branch', type=str, help='Branch the PR will be open to', default="", required=False)
     parser.add_argument('--files', type=str, help='Files to check', default="", required=False)
     parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
 

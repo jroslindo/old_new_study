82a95586052946723b9d6a91b88767f8e8e5b1d8
jroslindo <jroslindotab@gmail.com>
2024-10-22 10:24:04 -0300

    change

---
+++ codechecker.py
@@ -0,0 +1,20 @@
+import argparse
+import subprocess
+
+def main(args):
+    print(args)
+
+if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Teste")
+    
+    # Add arguments
+    parser.add_argument('compare_branch', type=str, help='Branch the PR will be open to')
+    parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Call the main function with the parsed arguments
+    
+    main(args)
